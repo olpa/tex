bf6e3804796c735a5c4288112584e2eb1d3c9975
Oleg Parashchenko <olpa@uucode.com>
2011-04-08 17:00:03 +0200

    Parsing of LaTeX file.

--- delta_debugging/latexdd.py
+++ delta_debugging/latexdd.py
@@ -2,7 +2,7 @@ tmpdir          = 'tmp'
 rundir_basename = 'run'
 latex_cmdline   = 'latex -interaction batchmode -output-directory ${RUNDIR} ${FILENAME} 2>&1 >${RUNDIR}/stdout.txt'
 
-import os, tempfile, string, sys
+import os, tempfile, string, sys, re
 
 #
 # Create a directory to run TeX. Design decision name:
@@ -26,7 +26,46 @@ def run_latex(rundir, filename):
   cmdline = string.Template(latex_cmdline).substitute(sub)
   os.system(cmdline)
 
+#
+# errors: everything what starts with '! ' in log
+#
+def collect_errors(rundir, tex_file):
+  logfile = os.path.join(rundir, os.path.splitext(os.path.basename(tex_file))[0] + '.log')
+  s_errors = ''
+  h = open(logfile)
+  for l in h:
+    if '! ' == l[:2]:
+      s_errors = s_errors + l
+  h.close()
+  return s_errors
+
+#
+# LaTeX file consist of parts:
+# * classname       - {} part of \documentclass
+# * classoptions    - [] part of \documentclass
+# * preamble        - everything after \documentclass{} till \begin{document}
+# * document        - inside \begin{document}...\end{document}
+# Parsing is fragile.
+#
+re_dclass = re.compile('\\\\documentclass(\\[([^]]*)\\])?\{([^}]*)\}\\s*')
+class LatexFile:
+  def __init__(self, fname):
+    h = open(fname)
+    s = h.read()
+    h.close()
+    m = re_dclass.search(s)
+    if not m:
+      raise Exception("\\documentclass not found")
+    self.classoptions = m.group(2)
+    s = s[m.end():]
+    pos = s.index('\\begin{document}')
+    self.preamble = s[:pos].strip()
+    pos2 = s.index('\\end{document}')
+    self.document = s[16+pos:pos2].strip()
+
 if '__main__' == __name__:
   fname = sys.argv[1]
-  rundir = create_run_dir(tmpdir, rundir_basename)
-  run_latex(rundir, fname)
+  #rundir = create_run_dir(tmpdir, rundir_basename)
+  #run_latex(rundir, fname)
+  #print collect_errors(rundir, fname)
+  lf = LatexFile(fname)
