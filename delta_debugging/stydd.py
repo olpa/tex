d90e86fa46477f2f3f14bd4a6523c3f29e6dedeb
Oleg Parashchenko <olpa@uucode.com>
2011-04-19 15:25:14 +0200

    DD is implemented.

--- delta_debugging/stydd.py
+++ delta_debugging/stydd.py
@@ -1,23 +1,72 @@
-import sys
+import os, sys
+import DD
+import runlatex
 
 class OneCharDeltaFile:
   def __init__(self, fname):
     h = open(fname)
-    s = h.read()
+    self.content = h.read()
     h.close()
-    self.deltas = zip(range(1, 1+len(s)), s)
 
   def get_deltas(self):
-    return self.deltas
+    return list(range(len(self.content)))
 
   def apply_deltas(self, deltas):
     s = ''
-    for (_, ch) in deltas:
-      s = s + ch
+    for i in deltas:
+      s = s + self.content[i]
     return s
 
+  def write_stream(self, h, deltas):
+    h.write(self.apply_deltas(deltas))
+
+  def write_file(self, fname, deltas):
+    h = open(fname, 'w')
+    self.write_stream(h, deltas)
+    h.close()
+
+class StyDD(DD.DD):
+  def __init__(self, tex_file, sty_file):
+    DD.DD.__init__(self)
+    self.tex_file = os.path.basename(tex_file)
+    self.sty_file = os.path.basename(sty_file)
+    self.ocdf = OneCharDeltaFile(sty_file)
+    self.master_errors = self.run_latex_return_errors(self.ocdf.get_deltas())
+
+  def run_latex_return_errors(self, deltas):
+    rundir = runlatex.create_run_dir()
+    self.ocdf.write_file(os.path.join(rundir, self.sty_file), deltas)
+    return runlatex.run_latex_collect_errors(rundir, self.tex_file)
+
+  def _test(self, deltas):
+    errors = self.run_latex_return_errors(deltas)
+    if '' == errors:
+      return self.PASS
+    if self.master_errors == errors:
+      return self.FAIL
+    return self.UNRESOLVED
+
+  def coerce(self, c):
+    if len(c) < 10:
+      s = str(c)
+    else:
+      s = str(c[:10]) + '........'
+    return s
+
+  def show_applied_delta(self, h, deltas):
+    self.ocdf.write_stream(h, deltas)
+
+  def get_deltas(self):
+    return self.ocdf.get_deltas()
+
 if '__main__' == __name__:
-  fname = sys.argv[1]
-  sty = OneCharDeltaFile(fname)
-  deltas = sty.get_deltas()
-  print sty.apply_deltas(deltas)
+  (tex_file, sty_file) = sys.argv[1:]
+  runlatex.guess_latex_tool(tex_file)
+  dd = StyDD(tex_file, sty_file)
+  print 'Master errors:'
+  print dd.master_errors
+  deltas = dd.get_deltas()
+  c = dd.ddmin(deltas)
+  print 'The 1-minimal failure-inducing sty input is:'
+  print '--------------------------------------------'
+  dd.show_applied_delta(sys.stdout, c)
