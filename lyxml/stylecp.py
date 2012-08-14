94bacdc3c15c331718a874add8ee9d5140fcef37
Oleg Parashchenko <olpa@uucode.com>
2012-08-14 12:30:06 +0200

    REading the header of the input file.

--- lyxml/stylecp.py
+++ lyxml/stylecp.py
@@ -3,6 +3,15 @@ usage = 'python stylecp.py from_file to_file style_mask'
 
 import sys
 
+def read_header(h):
+  s = ''
+  for l in h:
+    s = s + l
+    l = l.rstrip()
+    if '\\end_header' == l:
+      break
+  return s
+
 if len(sys.argv) not in (3,4):
   print >>sys.stderr, 'Usage: ' + usage
   sys.exit(-1)
@@ -12,3 +21,8 @@ if 4 == len(sys.argv):
   mask = sys.argv[3]
 else:
   mask = '*'
+
+h = open(in_file)
+s = read_header(h)
+print s
+h.close()
