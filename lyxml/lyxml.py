80e791f072b2f30c01bd0e956e9171833f313c39
Oleg Parashchenko <olpa@uucode.com>
2012-09-19 09:29:18 +0200

    Old code removed.

--- lyxml/lyxml.py
+++ lyxml/lyxml.py
@@ -7,52 +7,8 @@ import lyxparser
 lx_ns = 'http://getfo.org/lyxml/'
 template_file = os.path.join(os.path.dirname(__file__), 'template.lyx')
 
-#
-# LyX file format
-#
-# Paragraph style:
-# see lyx-2.0.3/src/Paragraph.cpp, Paragraph::write
-#
-# * \begin_layout name [annotations]
-# * Options
-# * For each child:
-# . - META_INSET: direct write, or begin_inset ... end_inset
-# . - '\':        \\backslash\n
-# . - '.':        drops the trailing whitespace while writing .lyx
-# * \end_layout
-#
-# Character style
-# see lyx-2.0.3/src/insets/InsetFlex.cpp, InsetCollapsable.cpp,
-# InsetText.cpp, Text.cpp
-#
-# 1. \begin_inset Flex name
-# 2. "status collapsed" or "status open"
-# 3. For each item in style: print. Usually it is a paragraph style
-#    of name "Plain" and option "Layout":
-#    3b. \begin_layout Plain Layout
-#        ... text ...
-#        \end_layout
-# 4 \end_inset
-# comes from
-# (1):  Paragraph::write, InsetFlex::write
-# (2):  InsetCollapsable::write
-# (3):  Text::write
-# (3b): Paragraph::write
-# (4):  Paragraph::write
-#
-# There are also \begin_deeper and \end_deeper
-#
-# More at
-# http://wiki.lyx.org/Devel/LyXFileFormatReverse
-
 # =========================================================
 # LyX to XML
-
-def html_escape(s):
-  s = s.replace('&', '&amp;')
-  s = s.replace('<', '&lt;')
-  s = s.replace('>', '&gt;')
-  return s
 
 def lyx2xml(in_file, out_file, blob_file):
   if '-' == in_file:
@@ -62,13 +18,14 @@ def lyx2xml(in_file, out_file, blob_file):
   if '-' == out_file:
     h_out = sys.stdout
   else:
-    h_out = open('et_' + out_file, 'w') # FIXME
+    h_out = open(out_file, 'w')
   lyx2xml_h(h_in, h_out, blob_file)
   if not (h_in == sys.stdin):
     h_in.close()
   if not (h_out == sys.stdout):
     h_out.close()
 
+# Not exactly as XML specification says, but should work for 99.999%
 def is_xml_name_char(ch, is_start_char=0):
   if (':' == ch) or ('_' == ch):
     return 1
@@ -99,13 +56,12 @@ def xml_safe_name(s):
 class XmlBuilder:
 
   def __init__(self):
-    #self.xmldoc = xml.etree.ElementTree.ElementTree()
     self.root  = xml.etree.ElementTree.Element('lx:lyx', {'xmlns:lx': 'http://getfo.org/lyxml/'})
     self.node  = self.root
     self.stack = []
 
   def header_line(self, l):
-    pass # FIXME
+    pass
 
   def begin_body(self):
     pass
@@ -171,129 +127,6 @@ def lyx2xml_h(h_in, h_out, blob_file):
   lp = lyxparser.LyXparser(xb, h_in)
   lp.parse()
   h_out.write(xml.etree.ElementTree.tostring(xb.root, 'utf-8'))
-
-def lyx2xml_h2(h_in, h_out, blob_file):
-  blob  = BlobWriter(h_out, blob_file)
-  stack = [('#dummy','#dummy')]
-  re_begin_layout = re.compile("^\\\\begin_layout (?P<ename>[^ ]+)\s*(?P<eann>.*)$")
-  re_begin_inset  = re.compile("^\\\\begin_inset (?P<eann>[^ ]+) (?P<ename>.*)$")
-  def begin_end_tag(l, name, ann, is_end):
-    is_not_plain  = ('Plain' != name) or ('Layout' != ann)
-    is_char_style = ann in ('Flex', 'script')
-    if len(ann) and not(is_char_style) and not(ann in ('Branch')):
-      if is_not_plain:
-        blob.write(l)
-    else:
-      blob.flush()
-      h_out.write('<')
-      if is_end:
-        h_out.write('/')
-      if 'Branch' == ann:
-        h_out.write('lx:branch')
-        if not is_end:
-          h_out.write(' name="%s"' % html_escape(name))
-      else:
-        if ann in ('script'):
-          name = 'lx:' + name
-        h_out.write(name)
-      if is_char_style and not(is_end) and ('script'!=ann):
-        h_out.write(' lx:ch="1"')
-      if is_end and is_not_plain and not(is_char_style):
-        h_out.write(">\n")
-      else:
-        h_out.write('>')
-  skip_lines = -1 # -1: till \begin_body, -2: till \end_inset, N: how much (after \begin_inset)
-  inset_level = 0
-  h_out.write("<lx:lyx xmlns:lx='%s'>\n" % lx_ns)
-  for l in h_in:
-    if skip_lines:
-      if skip_lines > 0:
-        skip_lines = skip_lines - 1
-        continue                                           # continue
-      blob.write(l)
-      if '\\begin_body' == l[:11]:
-        skip_lines = 0
-      elif '\\end_inset' == l[:10]:
-        inset_level = inset_level - 1
-        if not inset_level:
-          skip_lines = 0
-      elif '\\begin_inset' == l[:12]:
-        inset_level = inset_level + 1
-      continue                                             # continue
-    l = l.rstrip("\r\n")
-    if not len(l):
-      continue                                             # continue
-    ch = l[0]
-    if not('\\' == ch):
-      blob.flush()
-      h_out.write(html_escape(l))
-      continue                                             # continue
-    if '\\backslash' == l:
-      blob.flush()
-      h_out.write('\\')
-      continue                                             # continue
-    if ('\\end_layout' == l) or ('\\end_inset' == l):
-      (el_name, el_ann) = stack.pop()
-      begin_end_tag(l, el_name, el_ann, 1)
-      continue                                             # continue
-    m = re_begin_layout.match(l)
-    if not m:
-      m = re_begin_inset.match(l)
-      if m:
-        skip_lines = 1
-        if m.group('eann') not in ('Flex', 'Branch', 'script'):
-          skip_lines  = -2
-          inset_level = 1
-          blob.write(l+"\n")
-          continue                                         # continue
-    if m:
-      el_name = m.group('ename')
-      el_ann  = m.group('eann')
-      stack.append((el_name, el_ann))
-      begin_end_tag(l, el_name, el_ann, 0)
-      continue                                             # continue
-    if ('\\end_body' == l) or ('\\end_document' == l):
-      continue                                             # continue
-    blob.write(l+"\n")
-  blob.flush()
-  blob.close_db()
-  h_out.write("</lx:lyx>\n")
-
-re_empty = re.compile('^\s*$')
-
-class BlobWriter:
-  def __init__(self, h, fname):
-    self.h     = h
-    self.fname = fname
-    self.db    = None
-    self.blob  = cStringIO.StringIO()
-
-  def get_db(self):
-    if not self.db:
-      self.db = anydbm.open(self.fname, 'c')
-    return self.db
-
-  def close_db(self):
-    if self.db:
-      self.db.close()
-
-  def write(self, s):
-    self.blob.write(s)
-
-  def len(self):
-    return self.blob.tell()
-
-  def flush(self):
-    if not(self.blob.tell()):
-      return
-    s = self.blob.getvalue()
-    self.blob.close()
-    if not re_empty.match(s):
-      s_hash = hashlib.md5(s).hexdigest()
-      self.h.write('<?LyXblob %s?>' % s_hash)
-      db = self.get_db()
-      db[s_hash] = s
-    self.blob = cStringIO.StringIO()
 
 # =========================================================
 # XML to LyX
